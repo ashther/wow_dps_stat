0753965ebad6bddcd51ba97108d663518c02bb32
ashther <ashther@qq.com>
2020-12-23 17:14:37 +0800

    update data

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-Rscript -e "wcl=wow::WCL$new();wcl$get_report(n=30);saveRDS(wcl, 'wcl.rds')"
-Rscript -e "rmarkdown::render('index.Rmd', output_dir='docs', quiet=TRUE)"
-git add *
-git commit -m "update data"
-git push
+#!/usr/lib64/R/bin/Rscript
+wcl <- wow::WCL$new()
+wcl$get_report(n = 10)
+saveRDS(wcl, 'wcl.rds')
+rmarkdown::render('index.Rmd', output_dir = 'docs/', quiet = TRUE)
+system('git add *')
+system("git commit -m 'update data'")
+system('git push')
